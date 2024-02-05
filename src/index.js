e46dac13040f457318714e0b277273a97a9ea608
reim-weib-hr <157751121+reim-weib-hr@users.noreply.github.com>
2024-02-05 06:36:29 -0500

    rem unused var

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-
-import WeTransfer from './App';
 // import Logs from './Logs';
 
 const root = ReactDOM.createRoot(document.getElementById('sample_we_transsfer_view_'));
