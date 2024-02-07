6176d397fd45dd3554a76f48b83917042ebb8d10
reim-weib-hr <157751121+reim-weib-hr@users.noreply.github.com>
2024-02-07 04:00:08 -0500

    redeployed for dams

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 // import Logs from './Logs';
+import WeTransfer from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('sample_we_transsfer_view_'));
 root.render(  
   <React.StrictMode>
-    <div>
-      <div>exp</div>
-  </div>
+    // <div>
+    <WeTransfer />
+  // </div>
   </React.StrictMode>
 );
 reportWebVitals();
